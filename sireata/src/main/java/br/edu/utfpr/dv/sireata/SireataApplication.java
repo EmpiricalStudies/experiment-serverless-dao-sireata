f6e0b73cdd21c6af440e5c0112c6129429486683
Gabriel Costa Silva <gabriel@Gabriels-MacBook.local>
2018-12-01 10:47:24 -0200

    - Added Sireata essential classes

---
+++ sireata/src/main/java/br/edu/utfpr/dv/sireata/SireataApplication.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.dv.sireata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SireataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SireataApplication.class, args);
+	}
+}
